9f13eb7422bcca5cba391d8ff0fa201bbe3f78c7
dtt <t2dx.inc@gmail.com>
2022-09-23 02:26:42 +0700

    feat(i18n): improve brazillian-portuguese translations

--- projects/angular-ngrx-material-starter/src/app/app-routing.module.ts
+++ projects/angular-ngrx-material-starter/src/app/app-routing.module.ts
@@ -27,13 +27,6 @@ const routes: Routes = [
       )
   },
   {
-    path: 'examples',
-    loadChildren: () =>
-      import('./features/examples/examples.module').then(
-        (m) => m.ExamplesModule
-      )
-  },
-  {
     path: '**',
     redirectTo: 'about'
   }
