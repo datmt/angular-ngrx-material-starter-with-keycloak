a246e0c067b0110edf7e44b9464f86344a4d8eae
dtt <t2dx.inc@gmail.com>
2022-09-23 09:41:46 +0700

    bye husky

--- projects/angular-ngrx-material-starter/src/app/app.module.ts
+++ projects/angular-ngrx-material-starter/src/app/app.module.ts
@@ -6,20 +6,43 @@ import { CoreModule } from './core/core.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
+import { APP_INITIALIZER } from '@angular/core';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'https://sso.openexl.com/auth/',
+        realm: 'license-server-dev',
+        clientId: 'frontend-angular'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 @NgModule({
   imports: [
     // angular
     BrowserAnimationsModule,
     BrowserModule,
-
-    // core
     CoreModule,
-
-    // app
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ]
 })
 export class AppModule {}
